6c846f36588c527234e01a072a56059bf2e83a76
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-02 14:39:30 +0500

    Add the Factory method for Warrior

--- src/com/narxoz/rpg/factory/WarriorHeroFactory.java
+++ src/com/narxoz/rpg/factory/WarriorHeroFactory.java
@@ -1,5 +1,14 @@
 package com.narxoz.rpg.factory;
 
-public class WarriorHeroFactory {
+public class WarriorHeroFactory implements HeroComponentFactory {
+    @Override
+    public String[] createAbilities() {
+        return new String[] {"Slash", "Shield Block", "War Cry"};
+    }
+
+    @Override
+    public String[] createWeapon() {
+        return new String[] {"Sword", "Axe"};
+    }
     
 }
