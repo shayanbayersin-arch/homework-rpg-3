efa668444889f64049cb1010c279c3e61ff5c4d7
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-02 14:42:46 +0500

    Add abstract method Mage

--- src/com/narxoz/rpg/factory/MageHeroFactory.java
+++ src/com/narxoz/rpg/factory/MageHeroFactory.java
@@ -1,5 +1,14 @@
 package com.narxoz.rpg.factory;
 
-public class MageHeroFactory {
+public  class MageHeroFactory implements HeroComponentFactory {
+    @Override
+    public String[] createAbilities() {
+        return new String[] {"Fireball", "Teleport"};
+    }
+
+    @Override
+    public String[] createWeapon() {
+        return new String[] {"Staff", "Wand"};
+    }
     
 }
