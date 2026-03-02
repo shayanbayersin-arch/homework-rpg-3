16c2c9053c07b3702bb47a1b029971d98cbb9ee6
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-02 22:04:01 +0500

    Add the Enemy builder

--- src/com/narxoz/rpg/builder/EnemyBuilder.java
+++ src/com/narxoz/rpg/builder/EnemyBuilder.java
@@ -1,5 +1,34 @@
 package com.narxoz.rpg.builder;
 
+import com.narxoz.rpg.enemy.BaseEnemy;
+import com.narxoz.rpg.enemy.Enemy;
+import com.narxoz.rpg.loot.LootTable;
+
 public class EnemyBuilder {
-    
+
+    private final BaseEnemy enemy = new BaseEnemy();
+
+    public EnemyBuilder name(String name) {
+        enemy.setName(name);
+        return this;
+    }
+
+    public EnemyBuilder hp(int hp) {
+        enemy.setHp(hp);
+        return this;
+    }
+
+    public EnemyBuilder damage(int damage) {
+        enemy.setDamage(damage);
+        return this;
+    }
+
+    public EnemyBuilder loot(LootTable loot) {
+        enemy.setLootTable(loot);
+        return this;
+    }
+
+    public Enemy build() {
+        return enemy;
+    }
 }
