ce1adc4c8d988884d8126bfcf1977e83b5c5b5ee
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-02 16:39:14 +0500

    Add ability

--- src/com/narxoz/rpg/combat/Ability.java
+++ src/com/narxoz/rpg/combat/Ability.java
@@ -1,5 +1,16 @@
 package com.narxoz.rpg.combat;
 
-public class Ability {
+public abstract class Ability {
+    protected final String name;
+    protected final int power;
     
+    protected Ability(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
+    public abstract void use(Character target);
+
+    public String getName() {
+        return name;
+    }
 }
