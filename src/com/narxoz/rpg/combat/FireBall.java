8c909c92ff18b9e427f4accddc8c9fcf10e17810
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-02 18:33:26 +0500

    Add the abilities

--- src/com/narxoz/rpg/combat/FireBall.java
+++ src/com/narxoz/rpg/combat/FireBall.java
@@ -1,5 +1,14 @@
 package com.narxoz.rpg.combat;
 
-public class FireBall {
-    
+
+
+public class FireBall extends Ability {
+    public FireBall() {
+        super("Fireball", 30);
+    }
+    @Override
+    public void use(Character target) {
+        target.takeDamage(this.power);
+        System.out.println(power);
+    }
 }
--- src/com/narxoz/rpg/combat/IceSpike.java
+++ src/com/narxoz/rpg/combat/IceSpike.java
@@ -1,5 +1,13 @@
 package com.narxoz.rpg.combat;
 
-public class IceSpike {
+public class IceSpike extends Ability {
+    public IceSpike() {
+        super("Ice Spike", 15);
+    }
+    @Override
+    public void use(Character target) {
+        target.takeDamage(this.power);
+        System.out.println(power);
+    }
     
 }
