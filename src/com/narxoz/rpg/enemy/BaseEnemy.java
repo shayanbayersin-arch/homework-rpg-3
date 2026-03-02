785a0bb27dc7d23044f8eb83356d1e649fe7d478
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-02 22:02:24 +0500

    Add and Cange the enemy

---
+++ src/com/narxoz/rpg/enemy/BaseEnemy.java
@@ -0,0 +1,64 @@
+package com.narxoz.rpg.enemy;
+
+
+import com.narxoz.rpg.character.Character;
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.loot.LootTable;
+import com.narxoz.rpg.prototype.CloneableEnemy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseEnemy implements Enemy, CloneableEnemy {
+
+    private String name;
+    private int hp;
+    private int damage;
+    private LootTable lootTable;
+    private List<Ability> abilities = new ArrayList<>();
+
+    // ===== setters for Builder =====
+
+    public void setName(String name) { this.name = name; }
+    public void setHp(int hp) { this.hp = hp; }
+    public void setDamage(int damage) { this.damage = damage; }
+    public void setLootTable(LootTable lootTable) { this.lootTable = lootTable; }
+    public void setAbilities(List<Ability> abilities) { this.abilities = abilities; }
+
+    // ===== logic =====
+
+    @Override
+    public void attack(Character target) {
+        target.takeDamage(damage);
+        System.out.println(name + " attacks for " + damage);
+    }
+
+    @Override
+    public void takeDamage(int amount) {
+        hp = Math.max(0, hp - amount);
+    }
+
+    @Override
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    @Override
+    public Enemy cloneEnemy() {
+        BaseEnemy copy = new BaseEnemy();
+        copy.name = this.name;
+        copy.hp = this.hp;
+        copy.damage = this.damage;
+        copy.lootTable = this.lootTable;
+        copy.abilities = new ArrayList<>(this.abilities);
+        return copy;
+    }
+
+    // ===== getters =====
+
+    @Override public String getName() { return name; }
+    @Override public int getHealth() { return hp; }
+    @Override public int getDamage() { return damage; }
+    @Override public List<Ability> getAbilities() { return abilities; }
+    @Override public LootTable getLootTable() { return lootTable; }
+}
--- src/com/narxoz/rpg/enemy/Enemy.java
+++ src/com/narxoz/rpg/enemy/Enemy.java
@@ -1,8 +1,21 @@
 package com.narxoz.rpg.enemy;
 
+import com.narxoz.rpg.character.Character;
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.loot.LootTable;
+
+import java.util.List;
+
 public interface Enemy {
-    String getTitle();
+
+    void attack(Character target);
+    void takeDamage(int amount);
+    boolean isAlive();
+
+    String getName();
+    int getHealth();
     int getDamage();
-    void applyDamage(int amount);
-    boolean isDefeated();
+
+    List<Ability> getAbilities();
+    LootTable getLootTable();
 }
